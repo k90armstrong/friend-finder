356a37899b2082bd51d11de6ccc880d280371441
Kyle <k90armstrong@gmail.com>
2017-12-20 22:24:20 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+// create the express server called app
+var app = express();
+
+// set up the middleware
+app.use();
+
